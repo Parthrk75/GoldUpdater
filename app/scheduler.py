fccf903aedf28f59431db065c6bc78e74282333a
Parth Komawar <120014808+Parthrk75@users.noreply.github.com>
2025-01-27 05:19:27 +0530

    Add files via upload

---
+++ app/scheduler.py
@@ -0,0 +1,12 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from app.main import main
+
+# Initialize the scheduler
+scheduler = BlockingScheduler()
+
+# Schedule the `main` function to run every day at 12:00 PM UTC
+scheduler.add_job(main, 'cron', hour=10, minute=0)
+
+if __name__ == "__main__":
+    print("Scheduler started. Waiting for the next run...")
+    scheduler.start()
