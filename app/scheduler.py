46fb5b690d31bdfc6e5655d4808f746223abdece
Parth Komawar <120014808+Parthrk75@users.noreply.github.com>
2025-01-27 06:00:26 +0530

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,13 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-# scheduler.py
 from main import main  # Assuming 'main.py' is in the same 'app' directory
-
 
 # Initialize the scheduler
 scheduler = BlockingScheduler()
 
-# Schedule the `main` function to run every day at 12:00 PM UTC
-scheduler.add_job(main, 'cron', hour=10, minute=0)
+# Schedule the `main` function to run every day at 10:00 AM UTC
+scheduler.add_job(main, 'cron', hour=10, minute=0, second=0, timezone='UTC')
 
 if __name__ == "__main__":
     print("Scheduler started. Waiting for the next run...")
